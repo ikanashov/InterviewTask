70805192c4ea98d7270836b00e43636e6e7721e2
Ivan Kanashov <ikanashov@gmail.com>
2021-05-18 23:34:29 +0500

    video process

--- client/client.py
+++ client/client.py
@@ -1,8 +1,8 @@
 from datetime import timedelta
 from typing import Union
 
-from .config import DELTATIMEFORIMAGE, JPGEXTENSION
-from .models import ServerData
+from .config import ALLOWEDVIDEOEXTWEEKEND, ALLOWEDVIDEOEXTWORKDAY, DELTATIMEFORIMAGE, JPGEXTENSION
+from .models import AllowData, ServerData
 
 
 class Client():
@@ -25,7 +25,9 @@ class Client():
             return self.request.ts - timedelta(hours=DELTATIMEFORIMAGE)
    
     def process_video(self) -> str:
-        return 'video'
+        if self.request.ts.isoweekday() > 5:
+           return self.is_video_allow(self.request.content, ALLOWEDVIDEOEXTWEEKEND)
+        return self.is_video_allow(self.request.content, ALLOWEDVIDEOEXTWORKDAY)
     
     def process_text(self) -> str:
         return 'text'
@@ -41,6 +43,18 @@ class Client():
             return file_ext[0]
         else:
             return False
+
+    @staticmethod
+    def is_video_allow(filename: str, lenght: int) -> AllowData:
+        file_ext = filename.rsplit('.', maxsplit=1)
+        if (
+            (len(file_ext[-1]) == lenght)
+            and (file_ext[0] != '') 
+            and (len(file_ext) > 1)
+        ):
+            return AllowData.ok.value
+        return AllowData.reject.value
+
 
     def process_data(self) -> str:
         if self.request.type == self.request.type.sound:
